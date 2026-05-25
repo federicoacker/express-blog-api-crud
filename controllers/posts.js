6ec9e912b9995e3865c790d7e906ca03cb7751df
federicoacker <acker.federico@gmail.com>
2026-05-25 14:55:38 +0200

    Aggiunto funzionalità alla destroy

--- controllers/posts.js
+++ controllers/posts.js
@@ -81,11 +81,27 @@ function modify(request, response) {
 }
 
 function destroy(request, response) {
-    response.json({
-        error:null,
-        result: "Bravo hai fatto una delete"
-    })
 
+    const idReal = Number(request.params.id);
+    
+    if(isNaN(idReal) || idReal <= 0){
+        return response.status(400).json({
+            error:"L'id inserito non è nel formato corretto. Deve essere un numero intero positivo",
+            result: null
+        });
+    }
+
+    const postIndex = posts.findIndex(post => post.id === idReal);
+
+    if(postIndex === -1){
+        return response.status(404).json({
+            error: "Non ci sono post con quell'id",
+            result: null
+        })
+    }
+
+    posts.splice(postIndex, 1);
+    response.sendStatus(204);
 }
 
 export default postsController;
